7a37f1c753fa7d1339cb77fe7707f6ed4c10a71c
KhakimSeidalkhan <91054393+KhakimSeidalkhan@users.noreply.github.com>
2021-12-25 09:14:51 +0600

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,12 @@
+function onMouse(number) {
+    var x = document.getElementById("macBook");
+    if(number == 1){
+        x.innerHTML = "2400$";
+    }else if(number == 2){
+        x.innerHTML = "1100$";
+    }else if(number == 3){
+        x.innerHTML = "1000$";
+    }else if(number == 4){
+        x.innerHTML = "1500$";
+    }
+  }
